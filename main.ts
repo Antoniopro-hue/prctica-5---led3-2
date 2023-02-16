8c8e154cb9922f4f48c5b75fbd2f13eab06cc00b
Antoniopro-hue <63246009+Antoniopro-hue@users.noreply.github.com>
2023-02-16 14:01:05 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+let control2 = 0
 basic.forever(function () {
-	
+    for (let index = 0; index < 4; index++) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(100)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
+    basic.pause(100)
+    while (control2 < 1024) {
+        control2 += 1
+    }
 })
